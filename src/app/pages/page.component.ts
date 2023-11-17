0c18c0176259ef6e105d704acfa4accd0fe7a26d
Abner Antonio Chamorro Diaz <achamorro@MacBook-Pro-de-Usuario-3.local>
2023-11-17 17:56:07 -0500

    add routes and pageNotFound

---
+++ src/app/pages/page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styles: [
+  ]
+})
+export class PageComponent {
+
+}
